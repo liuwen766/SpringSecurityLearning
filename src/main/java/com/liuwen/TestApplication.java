d6f764e8711e84f5a0afb323d1854dba006d8288
liuwen766 <780442619@qq.com>
2020-04-06 01:30:54 +0800

    启动类

---
+++ src/main/java/com/liuwen/TestApplication.java
@@ -0,0 +1,16 @@
+package com.liuwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: Good good study,day day up!
+ * @author: Liu Wen
+ * @create: 2020-04-06 01:28
+ **/
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
